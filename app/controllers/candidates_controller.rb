f44ca06412a8a99457a36b48fa0eab6ffb0a157a
Jordan <jordan5011@yahoo.com.tw>
2017-01-16 14:53:47 +0800

    Implement new & create function

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,4 +2,22 @@ class CandidatesController < ApplicationController
 	def index
 		@Candidates = Candidate.all
 	end
+
+	def new
+		@Candidate = Candidate.new
+	end
+
+	def create
+		@Candidate = Candidate.new(candidate_params)
+
+		if @Candidate.save
+			redirect_to candidates_path, notice:"Create new candidate success."
+		else
+			render :new
+		end
+	end
+
+	def candidate_params
+		params.require(:candidate).permit(:name, :age, :party, :politics)
+	end
 end
